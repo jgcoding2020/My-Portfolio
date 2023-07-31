07e36e812b0db96e6be9ca8fcb350cba77deed9d
jgcoding2020 <jgardn11@yahoo.com>
2023-07-31 17:43:32 -0400

    Started MyProjects component additions

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -1,8 +1,21 @@
 import './MyProjects.css';
 
-function MyProjects () {
-    return(
-        <div id="my-projects">My Projects
+/**
+ * MyProjects function contains information about my projects pertaining to being a developer
+ * @returns div #my-projects
+ */
+function MyProjects() {
+    return (
+        <div id="my-projects">
+            {/* Heading at top of MyProjects page */}
+            <h1>What Have I Accomplished?</h1>
+            <h2 id="my-projects-heading">My Projects</h2><hr></hr>
+            <h4 id="user-forum-heading">User Forum</h4>
+            {/* User Forum section */}
+            <div id="user-forum-section">
+                <img id="user-forum-img"></img>
+                <article id="user-forum-article"></article>
+            </div>
         </div>
     );
 }
