31131cebda45d9ef3d3aeaf893b892ad2d4f452b
jgcoding2020 <jgardn11@yahoo.com>
2023-08-10 19:42:54 -0400

    Finished hobbies section and added gh-pages

--- src/pages/hobbies/Hobbies.js
+++ src/pages/hobbies/Hobbies.js
@@ -1,4 +1,3 @@
-import Iframe from 'react-iframe';
 import "./Hobbies.css";
 
 /**
@@ -8,40 +7,30 @@ import "./Hobbies.css";
 function Hobbies() {
     return (
         <div id="hobbies">
-            {/*Hobbies & Interests heading*/}
-            <div class="container-fluid text-center my-5" >
-                <div class="row">
-                    <h1 class="col text-light" id="history">Hobbies &amp; Interests</h1>
-                </div>
-            </div>
             {/*Hobbies section*/}
-            <div id="activities" class="container-fluid mb-5 text-center">
-                <div class="row bg-secondary text-light">
-                    <div class="col-sm-4">
+            <div id="activities" className="container-fluid bg-secondary m-5 mx-auto text-center text-light">
+                <h2 className="col mt-3 mb-5">Hobbies &amp; Interests</h2>
+                <div className="row">
+                    <div className="col-sm-4">
                         <p>Tennis</p><p>Watching Movies</p><p>Video Games</p>
                         <p>Road Cycling</p><p>Working Out</p><p>Running</p>
                     </div>
-                    <div class="col-sm-4">
+                    <div className="col-sm-4">
                         <p>Making Music</p><p>Basketball</p><p>Martial Arts</p>
                         <p>Pool &amp; Billiards</p><p>Baseball</p><p>Coding</p>
                     </div>
-                    <div class="col-sm-4">
+                    <div className="col-sm-4">
                         <p>Swimming</p><p>Playing Piano</p><p>Volleyball</p>
                         <p>Darts</p><p>Mountain Biking</p><p>Disc Golf</p>
                     </div>
+                    <a href="https://jgcoding2020.github.io/Piano-Portfolio-V2/" target="_blank" >
+                        <img id="piano" src={process.env.PUBLIC_URL + "images/piano.jpg"} alt="piano-image"></img>
+                        <img id="piano-logo" src={process.env.PUBLIC_URL + "images/piano-logo.jpg"} alt="piano-logo"></img>
+                    </a>
                 </div>
             </div>
-            {/*iframe containing piano portfollio website*/}
-            <Iframe url="https://jgcoding2020.github.io/Piano-Portfolio-V2/"
-                width="100%"
-                height="100%"
-                id="iframe"
-                className="iframe"
-                display="block"
-                position="relative" />
         </div>
     );
 }
 
 export default Hobbies;
-/*<img src={process.env.PUBLIC_URL + "images/myProjectsImages/webProjectPortalImages/image_0.png"} alt="under_construction.png"></img>*/
