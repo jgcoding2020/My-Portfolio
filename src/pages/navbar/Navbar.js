4ed58e69276cd7fbac06349e8ae7a89c4eb3d9fa
jgcoding2020 <jgardn11@yahoo.com>
2023-08-10 10:35:37 -0400

    modified Navbar buttons to access link on entire button

--- src/pages/navbar/Navbar.js
+++ src/pages/navbar/Navbar.js
@@ -11,16 +11,15 @@ function Navbar() {
         <>
             <nav id="nav">
                 <Link className="links" to="/"><img id="headshot" src={process.env.PUBLIC_URL + "images/navbarImages/headshot.PNG"} alt="Headshot"></img></Link>
-                <button type="button" className="btn btn-primary"><Link className="links" to="About">About Me</Link></button>
-                <button type="button" className="btn btn-primary"><Link className="links" to="MyProjects">Projects</Link></button>
-                <button type="button" className="btn btn-primary"><Link className="links" to="Skills">Skills</Link></button>
-                <button type="button" className="btn btn-primary"><Link className="links" to="ContactMe">Contact Me</Link></button>
-                <button type="button" className="btn btn-primary"><Link className="links" to="Hobbies">Hobbies & Interests</Link></button>
+                <Link className="links" to="About"><button type="button" className="btn btn-primary">About Me</button></Link>
+                <Link className="links" to="MyProjects"><button type="button" className="btn btn-primary">Projects</button></Link>
+                <Link className="links" to="Skills"><button type="button" className="btn btn-primary">Skills</button></Link>
+                <Link className="links" to="ContactMe"><button type="button" className="btn btn-primary">Contact Me</button></Link>
+                <Link className="links" to="Hobbies"><button type="button" className="btn btn-primary">Hobbies & Interests</button></Link>
             </nav>
             <Outlet></Outlet>
         </>
     );
-
 }
 
 export default Navbar;
