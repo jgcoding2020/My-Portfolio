b1ea0be73693c0dbded1fdfeb130d0d71e1c22b5
jgcoding2020 <jgardn11@yahoo.com>
2023-08-11 16:49:27 -0400

    Added links to images on click

--- src/pages/myProjects/doConnect/DoConnectSlider.js
+++ src/pages/myProjects/doConnect/DoConnectSlider.js
@@ -22,43 +22,43 @@ const DoConnectSlider = () => {
   return (
     <Slider {...settings}>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_1.png`} alt="image_1.png" />
+        <a href={`${doConnectImagePath}image_1.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_1.png`} alt="image_1.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_2.png`} alt="image_2.png" />
+        <a href={`${doConnectImagePath}image_2.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_2.png`} alt="image_2.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_3.png`} alt="image_3.png" />
+        <a href={`${doConnectImagePath}image_3.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_3.png`} alt="image_3.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_4.png`} alt="image_4.png" />
+        <a href={`${doConnectImagePath}image_4.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_4.png`} alt="image_4.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_5.png`} alt="image_5.png" />
+      <a href={`${doConnectImagePath}image_5.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_5.png`} alt="image_5.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_6.png`} alt="image_6.png" />
+      <a href={`${doConnectImagePath}image_6.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_6.png`} alt="image_6.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_7.png`} alt="image_7.png" />
+      <a href={`${doConnectImagePath}image_7.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_7.png`} alt="image_7.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_8.png`} alt="image_8.png" />
+      <a href={`${doConnectImagePath}image_8.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_8.png`} alt="image_8.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_9.png`} alt="image_9.png" />
+      <a href={`${doConnectImagePath}image_9.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_9.png`} alt="image_9.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_10.png`} alt="image_10.png" />
+      <a href={`${doConnectImagePath}image_10.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_10.png`} alt="image_10.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_11.png`} alt="image_11.png" />
+      <a href={`${doConnectImagePath}image_11.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_11.png`} alt="image_11.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_12.png`} alt="image_12.png" />
+      <a href={`${doConnectImagePath}image_12.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_12.png`} alt="image_12.png" /></a>
       </div>
       <div>
-        <img decoding="async" src={`${doConnectImagePath}image_13.png`} alt="image_13.png" />
+      <a href={`${doConnectImagePath}image_13.png`} target="_blank"><img decoding="async" src={`${doConnectImagePath}image_13.png`} alt="image_13.png" /></a>
       </div>
     </Slider>
   );
