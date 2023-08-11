30c076f8d0b666a5d84ea492ac76c9cfbf59e1ce
jgcoding2020 <jgardn11@yahoo.com>
2023-08-10 21:07:56 -0400

    file path change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css';
  */
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       <main id="main">
         <Navbar></Navbar>
         <Routes>
