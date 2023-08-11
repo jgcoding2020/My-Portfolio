6b3534b5f01c6cc0b278c903381383d79b891268
jgcoding2020 <jgardn11@yahoo.com>
2023-08-10 21:29:43 -0400

    changed to file paths

--- src/pages/navbar/Navbar.js
+++ src/pages/navbar/Navbar.js
@@ -11,11 +11,11 @@ function Navbar() {
         <>
             <nav id="nav">
                 <Link className="links" to="/"><img id="headshot" src={process.env.PUBLIC_URL + "images/navbarImages/headshot.PNG"} alt="Headshot"></img></Link>
-                <Link className="links" to="About"><button type="button" className="btn btn-primary">About Me</button></Link>
-                <Link className="links" to="MyProjects"><button type="button" className="btn btn-primary">Projects</button></Link>
-                <Link className="links" to="Skills"><button type="button" className="btn btn-primary">Skills</button></Link>
-                <Link className="links" to="ContactMe"><button type="button" className="btn btn-primary">Contact Info</button></Link>
-                <Link className="links" to="Hobbies"><button type="button" className="btn btn-primary">Hobbies & Interests</button></Link>
+                <Link className="links" to="/About"><button type="button" className="btn btn-primary">About Me</button></Link>
+                <Link className="links" to="/MyProjects"><button type="button" className="btn btn-primary">Projects</button></Link>
+                <Link className="links" to="/Skills"><button type="button" className="btn btn-primary">Skills</button></Link>
+                <Link className="links" to="/ContactMe"><button type="button" className="btn btn-primary">Contact Info</button></Link>
+                <Link className="links" to="/Hobbies"><button type="button" className="btn btn-primary">Hobbies & Interests</button></Link>
             </nav>
             <Outlet></Outlet>
         </>
